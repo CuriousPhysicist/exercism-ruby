24bb192d096593137d14ecc15f217ed602f6be90
CuriousPhysicist <andrewhampson6@gmail.com>
2017-09-22 22:25:35 +0100

    Gigasecond challenge V6: All tests passing, ready to submit.

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -1,12 +1,11 @@
 class Gigasecond
   def self.from(date)
-
-  	start = date.to_i
-  	finish = start + 10**9
-  	Time.at(finish)
-  	# puts finish
-
-
-
+    start = date.to_i
+    finish = start + 10**9
+    Time.at(finish)
   end
+end
+
+module BookKeeping
+  VERSION = 6
 end
