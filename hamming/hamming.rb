cc9483e0963b59191953798636521bbdea5f3dcd
CuriousPhysicist <andrewhampson6@gmail.com>
2017-09-22 15:37:11 +0100

    Hamming challenge: Fith test passing, conditionals creeping in...

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,5 +1,15 @@
 class Hamming
 	def self.compute(strand1, strand2)
-		strand1 == strad2 ? 0 : 1
+		if strand1 == strand2 
+			0
+		else 
+			counter = 0
+			(0..strand1.length-1).each do |i|
+				if strand1[i] != strand2[i] then
+					counter += 1
+				end
+			end
+			counter
+		end
 	end
 end
