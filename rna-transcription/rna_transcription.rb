b18ec144a1bd8ca50abb96056bff7e44eef14e7f
CuriousPhysicist <andrewhampson6@gmail.com>
2017-09-23 18:43:04 +0100

    RNA challenge: 4 tsts passing, case statement form of code.

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,5 +1,20 @@
 class Complement
   def self.of_dna(strand)
-    "G"
+    
+    result = ""
+
+    case
+      when strand == "C"
+        result = "G"
+      when strand == "G"
+        result = "C"
+      when strand == "A"
+        result = "U"
+      when strand == "T"
+        result = "A"
+    end
+
+    result
+
   end
 end
