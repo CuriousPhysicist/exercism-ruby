c30d2d5f5f60300e3e394ba5209e059537465683
CuriousPhysicist <andrewhampson6@gmail.com>
2017-09-24 21:59:27 +0100

    RNA challenge: Refactored to Extract Class, all tests are green.

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,11 +1,47 @@
+# initial form for first 2 iterations
+# class Complement
+#   def self.of_dna(strand)
+    
+#     return '' if strand == ''
+
+#     unzipped_dna = strand.split("")
+
+#     build_rna_from(unzipped_dna)
+
+#   end
+
+#   def self.build_rna_from(dna_array)
+
+#     rna_array = dna_array.map do |base|
+
+#       return '' unless %w[A T C G].one? { |b| b == base }
+
+#       case
+#         when base == "C"
+#           result = "G"
+#         when base == "G"
+#           result = "C"
+#         when base == "A"
+#           result = "U"
+#         when base == "T"
+#           result = "A"
+#       end
+#     end
+
+#     rna_array.join
+
+#   end
+# end
+
+# Refactor to ensure single responsibility and make more OO in design
+
 class Complement
+  
   def self.of_dna(strand)
     
     return '' if strand == ''
 
     unzipped_dna = strand.split("")
-
-    # return '' unless unzipped_dna.all? { |base| %w[A T C G].one? { |b| b == base } }
 
     build_rna_from(unzipped_dna)
 
@@ -17,21 +53,30 @@ class Complement
 
       return '' unless %w[A T C G].one? { |b| b == base }
 
-      case
-        when base == "C"
-          result = "G"
-        when base == "G"
-          result = "C"
-        when base == "A"
-          result = "U"
-        when base == "T"
-          result = "A"
-      end
+      RNA.find_match_for(base)
+      
     end
 
-  rna_array.join
-
+    rna_array.join
+    
   end
+end
+
+class RNA
+
+  def self.find_match_for(base)
+    case
+      when base == "C"
+        return "G"
+      when base == "G"
+        return "C"
+      when base == "T"
+        return "A"
+      when base == "A"
+        return "U"
+    end
+  end
+
 end
 
 module BookKeeping
