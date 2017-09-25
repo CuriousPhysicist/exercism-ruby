aaa78c88dca275cc59ca735595341c9e4b10f7e7
CuriousPhysicist <andrewhampson6@gmail.com>
2017-09-25 19:50:16 +0100

    RNA challenge: Cleaned up the code and removed another conditional.

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,61 +1,20 @@
-# initial form for first 2 iterations
-# class Complement
-#   def self.of_dna(strand)
-    
-#     return '' if strand == ''
-
-#     unzipped_dna = strand.split("")
-
-#     build_rna_from(unzipped_dna)
-
-#   end
-
-#   def self.build_rna_from(dna_array)
-
-#     rna_array = dna_array.map do |base|
-
-#       return '' unless %w[A T C G].one? { |b| b == base }
-
-#       case
-#         when base == "C"
-#           result = "G"
-#         when base == "G"
-#           result = "C"
-#         when base == "A"
-#           result = "U"
-#         when base == "T"
-#           result = "A"
-#       end
-#     end
-
-#     rna_array.join
-
-#   end
-# end
-
-# Refactor to ensure single responsibility and make more OO in design
-
 class Complement
   
   def self.of_dna(strand)
-    
-    return '' if strand == ''
 
     unzipped_dna = strand.split("")
+
+    return '' unless unzipped_dna.all? { |base| %w[A T C G].one? { |b| b == base } }
 
     build_rna_from(unzipped_dna)
 
   end
 
+  private
+
   def self.build_rna_from(dna_array)
 
-    rna_array = dna_array.map do |base|
-
-      return '' unless %w[A T C G].one? { |b| b == base }
-
-      RNA.find_match_for(base)
-      
-    end
+    rna_array = dna_array.map { |base| RNA.find_match_for(base) } 
 
     rna_array.join
     
