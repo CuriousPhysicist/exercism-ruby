984cf198d78e41fdbabdd4199d2d2fdb55d2c213
CuriousPhysicist <andrewhampson6@gmail.com>
2017-09-26 15:08:37 +0100

    RNA Challenge: New DNA object given responsibilities and code refactored.

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,55 +1,44 @@
 class Complement
-
   def self.of_dna(strand)
-    dna = DNA.new(strand)
-    build_rna_from(dna.strand)
-  end
-
-  class << self
-    private
-    def build_rna_from(dna)
-      rna = dna.map { |base| RNA.find_match_for(base) }
-      rna.join
-    end
+    DNA.new(strand).convert_to_rna
   end
 end
 
 class DNA
-  
-  def initialize (dna)
-    unzip (dna)
+  def initialize(nucleotides)
+    @dna = unzip nucleotides
   end
-  
-  def strand
-    @dna
+
+  def convert_to_rna
+    rna = @dna.map { |base| RNA.find_match_for(base) }
+    rna.join
   end
   
   private
   
-  def unzip (dna)
-    unzipped_dna = dna.split("")
-    if unzipped_dna.all? { |base| %w[A T C G].one? { |b| b == base } } then
-      @dna = unzipped_dna
+  def unzip(dna)
+    unzipped_dna = dna.split('')
+    if unzipped_dna.all? { |base| %w[A T C G].one? { |b| b == base } }
+      unzipped_dna
     else
-      @dna = []
+      []
     end
   end
 end
 
 class RNA
-
   def self.find_match_for(base) 
     case base
-      when "C"
-        "G"
-      when "G"
-        "C"
-      when "T"
-        "A"
-      when "A"
-        "U"
-      else
-        raise "Unexpected input for RNA, #{base}"
+    when 'C'
+      'G'
+    when 'G'
+      'C'
+    when 'T'
+      'A'
+    when 'A'
+      'U'
+    else
+      raise "Unexpected input for RNA, #{base}"
     end
   end
 end
