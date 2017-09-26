cb1a726e279ea41ff1954063d8646c08214bde77
CuriousPhysicist <andrewhampson6@gmail.com>
2017-09-26 20:51:56 +0100

    RNA challenge: Extracted the unzip method into separate class.

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,28 +1,44 @@
 class Complement
   def self.of_dna(strand)
-    DNA.new(strand).convert_to_rna
+    genome = Genome.new(strand)
+    DNA.new(genome.strand).convert_to_rna
   end
 end
 
-class DNA
+class Genome
   def initialize(nucleotides)
-    @dna = unzip nucleotides
+    @strand = unzip nucleotides
   end
-
-  def convert_to_rna
-    rna = @dna.map { |base| RNA.find_match_for(base) }
-    rna.join
+  
+  def strand
+    @strand
   end
   
   private
   
   def unzip(dna)
     unzipped_dna = dna.split('')
+    
     if unzipped_dna.all? { |base| %w[A T C G].one? { |b| b == base } }
       unzipped_dna
     else
-      []
+      raise "Input strand #{dna} is not a valid genome sequence."
     end
+    
+  rescue => e
+    puts "#{e}"
+    []
+  end
+end
+
+class DNA
+  def initialize(dna)
+    @dna = dna
+  end
+
+  def convert_to_rna
+    rna = @dna.map { |base| RNA.find_match_for(base) }
+    rna.join
   end
 end
 
@@ -46,3 +62,4 @@ end
 module BookKeeping
   VERSION = 4
 end
+
