f5c354bb6ce4a0369982e46263c836f2c9f212e2
CuriousPhysicist <andrewhampson6@gmail.com>
2017-09-27 21:42:37 +0100

    RNA challenge: Single responsibility classes, all tests green and Rubocop happy! :-)

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,51 +1,58 @@
+# Primary class used to recieve and prepare the input strand for processing
 class Complement
   def self.of_dna(strand)
-    @unzipped_strand = unzip strand
-    DNA.new(@unzipped_strand).convert_to_rna
+    unzipped_strand = unzip strand
+    DNA.new(unzipped_strand).convert_to_rna
   end
-
   class << self
     private
+
     def unzip(strand)
       strand.split('')
     end
   end
 end
 
+# This class checks the validity of the input strand
 class DNA
   def initialize(unzipped_strand)
     @dna = unzipped_strand
     check_strand_is_dna @dna
   end
 
-  def check_strand_is_dna(unzipped_strand)
-    @dna = [] unless unzipped_strand.all? { |base| %w[A T C G].one? { |b| b == base } }
+  def convert_to_rna
+    RNA.new.convert(@dna)
   end
 
-  def convert_to_rna
-    rna = @dna.map { |base| RNA.find_match_for(base) }
-    rna.join
+  private
+
+  def check_strand_is_dna(unzipped_strand)
+    @dna = [] unless unzipped_strand.all? do |base|
+      %w[A T C G].one? { |b| b == base }
+    end
   end
 end
 
+# This class performs the conversion on a valid strand
 class RNA
-  def self.find_match_for(base) 
-    case base
-    when 'C'
-      'G'
-    when 'G'
-      'C'
-    when 'T'
-      'A'
-    when 'A'
-      'U'
-    else
-      raise "Unexpected input for RNA, #{base}"
-    end
+  def initialize
+    @option = { C: 'G', G: 'C', T: 'A', A: 'U' }
+  end
+
+  def convert(dna)
+    rna = dna.map { |base| find_match_for(base) }
+    rna.join
+  end
+
+  private
+
+  def find_match_for(base)
+    @option.fetch base.to_sym
+  rescue KeyError
+    raise "Unexpected input for RNA, #{base}"
   end
 end
 
 module BookKeeping
   VERSION = 4
 end
-
