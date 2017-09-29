70197a45a6b0f0b4d455a570a7d57e62a43f2f8b
CuriousPhysicist <andrewhampson6@gmail.com>
2017-09-29 08:50:05 +0100

    Raindrops challenge: Shameless Green version.

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,13 +1,26 @@
 class Raindrops
   def self.convert(number)
-    if number == 3
-      'Pling'
-    elsif number == 5
-      'Plang'
-    elsif number == 7
-      'Plong'
-    else
+    
+    result = ""
+    
+    if number % 3 == 0
+      result += "Pling"
+    end
+    if number % 5 == 0
+      result += "Plang"
+    end
+    if number % 7 == 0
+      result += "Plong"
+    end
+
+    if result == ""
       number.to_s
+    else
+      result
     end
   end
+end
+
+module BookKeeping
+  VERSION = 3
 end
