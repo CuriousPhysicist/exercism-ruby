fc8813d22ba0658d3035d2413b9e7f29d51ef9ec
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-09 20:54:20 +0100

    Roman Numerals challenge: Immutable objects first try...

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,25 +1,46 @@
-# Required Class
+# Integer Monkey Patch
 class Integer
   def to_roman
     key_numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     numerals = %w[M CM D CD C XC L XL X IX V IV I]
-    convertor(key_numbers, numerals)
+    Encoder.new(key_numbers, numerals).encode(self)
+  end
+end
+
+# Class for encoding integers to arbirary code
+class Encoder
+  attr_reader :encoding_hash, :pair, :candidate, :cache
+  def initialize(keys, values)
+    @encoding_hash = keys.zip(values).to_h
+    @pair = Struct.new(:number, :code)
+    @cache = translation_cache
   end
 
-  def convertor(key_numbers, numerals)
-    tracker = self
-    result = ''
-    translation(key_numbers, numerals).each do |key, value|
-      tracker.div(key).times do
-        result += value
-        tracker -= key
+  def encode(number)
+    create_candidate(number)
+    encode_number
+  end
+
+  private
+
+  def create_candidate(number, code = '')
+    @candidate = pair.new(number, code)
+  end
+
+  def encode_number
+    cache.each do |t|
+      next if candidate.number < t.number
+      candidate.number.div(t.number).times do
+        create_candidate(candidate.number - t.number, candidate.code + t.code)
       end
     end
-    result
+    candidate.code
   end
 
-  def translation(key_numbers, numerals)
-    key_numbers.zip(numerals).to_h
+  def translation_cache
+    encoding_hash.map do |key, value|
+      pair.new(key, value)
+    end
   end
 end
 
