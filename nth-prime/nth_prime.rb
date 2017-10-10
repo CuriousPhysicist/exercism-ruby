6d80252d1fb182d642e97629fc6fe298452d70dd
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-10 23:04:38 +0100

    Nth Prime: Rubocop compliant, Flog 18.6

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -2,16 +2,16 @@
 class Prime
   def self.nth(required_prime)
     raise ArgumentError if required_prime < 1
-    primes = sieve(1000000)
-    primes[required_prime-1]
+    primes = sieve(1_000_000)
+    primes[required_prime - 1]
   end
 
   def self.sieve(max)
     numbers = [*(2..max)]
     numbers.each do |n|
       next unless n
-      break if n*n > max
-      (n*n).step(max, n) { |num| numbers[num-2] = nil }
+      break if n * n > max
+      (n * n).step(max, n) { |num| numbers[num - 2] = nil }
     end
     numbers.compact
   end
