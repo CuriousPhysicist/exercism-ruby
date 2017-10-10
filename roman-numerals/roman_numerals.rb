3e3f3f054e4181b08aad87f2de2e9b2d02f34b55
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-10 09:10:56 +0100

    Roman Numerals Challenge: Flog score of 24, hard to see where to go next.

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,35 +1,29 @@
 # Integer Monkey Patch
 class Integer
   def to_roman
+    raise "The Romans didn't understand #{self}" if self < 1
     key_numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     numerals = %w[M CM D CD C XC L XL X IX V IV I]
-    Encoder.new(key_numbers, numerals).encode(self)
+    Converter.new(key_numbers, numerals).convert(self)
   end
 end
 
-# Class for encoding integers to arbirary code
-class Encoder
-  attr_reader :encoding_hash, :pair, :candidate
+# Class for converting integers to arbitrary code
+class Converter
+  attr_reader :encoding_hash
   def initialize(keys, values)
     @encoding_hash = keys.zip(values).to_h
-    @candidate = Struct.new(:number, :code)
   end
 
-  def encode(number)
-    encode_number(candidate.new(number, ''))
-  end
-
-  private
-
-  def encode_number(candidate)
+  def convert(number, code = '')
     encoding_hash.each do |key, value|
-      next if candidate.number < key
-      candidate.number.div(key).times do
-        candidate.number -= key
-        candidate.code += value
+      next if number < key
+      number.div(key).times do
+        number -= key
+        code += value
       end
     end
-    candidate.code
+    code
   end
 end
 
