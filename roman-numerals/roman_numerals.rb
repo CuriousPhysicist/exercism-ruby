c2b16b9b3d7ea45d82b50e29ee1f977b7f141f21
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-10 21:09:11 +0100

    Roman Numerals challenge: Functional programming approach??

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -2,28 +2,22 @@
 class Integer
   def to_roman
     raise "The Romans didn't understand #{self}" if self < 1
-    key_numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    numerals = %w[M CM D CD C XC L XL X IX V IV I]
-    Converter.new(key_numbers, numerals).convert(self)
+    numerals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1].zip(
+      %w[M CM D CD C XC L XL X IX V IV I]
+    ).to_h
+    Converter.new.convert(self, numerals)
   end
 end
 
 # Class for converting integers to arbitrary code
 class Converter
-  attr_reader :encoding_hash
-  def initialize(keys, values)
-    @encoding_hash = keys.zip(values).to_h
-  end
-
-  def convert(number, code = '')
+  def convert(number, encoding_hash, result = '')
     encoding_hash.each do |key, value|
       next if number < key
-      number.div(key).times do
-        number -= key
-        code += value
-      end
+      number.divmod(key)[0].times { result += value }
+      number = number.remainder(key)
     end
-    code
+    result
   end
 end
 
