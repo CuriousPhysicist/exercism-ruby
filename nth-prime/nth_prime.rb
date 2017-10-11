600128c5a006a85f8fffb887020e3357550a55c8
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-11 15:07:23 +0100

    Nth Prime: Seeker form of code.

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,13 +1,23 @@
 # Required Class
 class Prime
   def self.nth(required_prime)
-    raise ArgumentError if required_prime < 1
-    primes = sieve(1_000_000)
-    primes[required_prime - 1]
+    raise ArgumentError, 'Value must be greater than 0' if required_prime < 1
+    @primes = []
+    min = 2
+    max = 1_000_000
+    while @primes.size < required_prime
+      @primes += sieve(min, max)
+      min = max + 1
+      max *= 10
+    end
+    @primes[required_prime - 1]
   end
 
-  def self.sieve(max)
-    numbers = [*(2..max)]
+  def self.sieve(min, max)
+    numbers = [*(min..max)]
+    numbers.reject! do |n|
+      @primes.any? { |prime| (n % prime).zero? }
+    end
     numbers.each do |n|
       next unless n
       break if n * n > max
