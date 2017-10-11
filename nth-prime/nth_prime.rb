53a1863ac4dcbf5f3bce08c0759168200b5130b1
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-11 21:16:01 +0100

    Nth Prime challenge: Fog = 37, Rubocop happt, and now passing Reek.

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -3,27 +3,40 @@ class Prime
   def self.nth(required_prime)
     raise ArgumentError, 'Value must be greater than 0' if required_prime < 1
     @primes = []
-    min = 2
-    max = 1_000_000
-    while @primes.size < required_prime
-      @primes += sieve(min, max)
-      min = max + 1
-      max *= 10
-    end
+    hunt_for_primes(2, 1_000_000, required_prime)
     @primes[required_prime - 1]
   end
 
-  def self.sieve(min, max)
-    numbers = [*(min..max)]
-    numbers.reject! do |n|
-      @primes.any? { |prime| (n % prime).zero? }
+  def self.hunt_for_primes(min = 2, max = 1_000_000, required_prime)
+    while @primes.size < required_prime
+      @primes += filter(min, max)
+      min = max + 1
+      max *= 10
     end
-    numbers.each do |n|
-      next unless n
-      break if n * n > max
-      (n * n).step(max, n) { |num| numbers[num - 2] = nil }
+  end
+
+  def self.filter(min, max)
+    numbers = [*(min..max)].reject { |candidate| prime_test(candidate) }
+    sieve(numbers, max)
+  end
+
+  def self.prime_test(candidate)
+    @primes.any? { |prime| (candidate % prime).zero? }
+  end
+
+  def self.sieve(numbers, max)
+    numbers.each do |candidate|
+      next unless candidate
+      break if candidate * candidate > max
+      remove_multiples(candidate, max, numbers)
     end
     numbers.compact
+  end
+
+  def self.remove_multiples(candidate, max, numbers)
+    (candidate * candidate).step(max, candidate) do |num|
+      numbers[num - 2] = nil
+    end
   end
 end
 
