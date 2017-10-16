44eda04e2a950f07d07ba89fa800ffc186c18b7c
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-16 16:39:00 +0100

    Leap challenge: First iteration complete.

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -3,39 +3,43 @@ class Prime
   def self.nth(required_prime)
     raise ArgumentError, 'Value must be greater than 0' if required_prime < 1
     @primes = []
-    hunt_for_primes(2, 1_000_000, required_prime)
+    hunt_for_primes(required_prime, 2, 1_000_000)
     @primes[required_prime - 1]
   end
 
-  def self.hunt_for_primes(min = 2, max = 1_000_000, required_prime)
-    while @primes.size < required_prime
-      @primes += filter(min, max)
-      min = max + 1
-      max *= 10
+  class << self
+    private
+
+    def hunt_for_primes(required_prime, min = 2, max = 1_000_000)
+      while @primes.size < required_prime
+        @primes += filter(min, max)
+        min = max + 1
+        max *= 10
+      end
     end
-  end
 
-  def self.filter(min, max)
-    numbers = [*(min..max)].reject { |candidate| prime_test(candidate) }
-    sieve(numbers, max)
-  end
-
-  def self.prime_test(candidate)
-    @primes.any? { |prime| (candidate % prime).zero? }
-  end
-
-  def self.sieve(numbers, max)
-    numbers.each do |candidate|
-      next unless candidate
-      break if candidate * candidate > max
-      remove_multiples(candidate, max, numbers)
+    def filter(min, max)
+      numbers = [*(min..max)].reject { |candidate| prime_test(candidate) }
+      sieve(numbers, max)
     end
-    numbers.compact
-  end
 
-  def self.remove_multiples(candidate, max, numbers)
-    (candidate * candidate).step(max, candidate) do |num|
-      numbers[num - 2] = nil
+    def prime_test(candidate)
+      @primes.any? { |prime| (candidate % prime).zero? }
+    end
+
+    def sieve(numbers, max)
+      numbers.each do |candidate|
+        next unless candidate
+        break if candidate * candidate > max
+        remove_multiples(candidate, max, numbers)
+      end
+      numbers.compact
+    end
+
+    def remove_multiples(candidate, max, numbers)
+      (candidate * candidate).step(max, candidate) do |num|
+        numbers[num - 2] = nil
+      end
     end
   end
 end
--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -2,7 +2,7 @@
 module Enumerable
   def contains_the_alphabet?
     alphabet = [*'a'..'z']
-    (alphabet - self).empty?
+    (alphabet - [*self].flatten).empty?
   end
 end
 
@@ -16,3 +16,62 @@ end
 module BookKeeping
   VERSION = 5
 end
+
+# Test class  for testing generality of the Enumerable monkey patch
+class MyCollection
+  include(Enumerable)
+  attr_reader :arguments
+  def initialize(*args)
+    @arguments = args
+    @counter = 0
+  end
+
+  def each(&block)
+    while @counter < arguments.length
+      @counter < arguments.length ? @counter += 1 : @counter = 0
+      if block
+        yield arguments[@counter - 1]
+      else
+        arguments[@counter - 1]
+      end
+    end
+  end
+end
+
+# Test Suite
+puts 'MyCollection numerical'
+p MyCollection.new(1, 2, 3, 4, 5).arguments
+puts 'test #each behaviour with Block using puts element'
+MyCollection.new(1, 2, 3, 4, 5).each { |c| puts c }
+puts 'test #each behaviour with Block using puts element^2'
+MyCollection.new(1, 2, 3, 4, 5).each { |c| puts c * 2 }
+puts 'applying the alphabet test, expect false'
+p MyCollection.new(1, 2, 3, 4, 5).contains_the_alphabet?
+
+puts "\nMyCollection Letters"
+p MyCollection.new('a', 'b', 'c', 'd', 'e').arguments
+puts 'applying the alphabet test, expect false'
+p MyCollection.new('a', 'b', 'c', 'd', 'e').contains_the_alphabet?
+
+puts "\nTesting #contains_the_alphabet? with an Array"
+p [*'a'..'z']
+p [*'a'..'z'].contains_the_alphabet?
+puts 'Testing #contains_the_alphabet? with an Array mutating each element'
+temp = (('a'..'z').map { |element| element + 'x' })
+p temp
+p temp.contains_the_alphabet?
+
+puts "\nTesting #contains_the_alphabet? with a Range"
+temp = ('a'..'z')
+p temp
+p temp.contains_the_alphabet?
+
+puts "\nTesting #contains_the_alphabet? with a Hash"
+p temp2 = [*1..26].zip([*'a'..'z']).to_h
+p temp2.contains_the_alphabet?
+p temp2 = [*'a'..'z'].zip([*1..26]).to_h
+p temp2.contains_the_alphabet?
+
+puts "\nTesting #contains_the_alphabet? with MyCollection"
+p temp = MyCollection.new(*'a'..'z')
+p temp.contains_the_alphabet?
