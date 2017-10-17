de686350ad18943edcc2f842f6a450839d391b58
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-17 08:50:49 +0100

    Update ruby folders.

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,7 +1,7 @@
 # Required Class
 class Year
   def self.leap?(year)
-    year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0) ? true : false
+    year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0)
   end
 end
 
