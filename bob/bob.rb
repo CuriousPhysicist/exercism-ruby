ac3d0ef74a4704df203d180be861c970e376c893
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-26 21:30:03 +0100

    Bob challenge: First step on OO path, still got the conditional...

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,14 +1,32 @@
 # The Primary Class
 class Bob
+
+  RESPONSE = {
+    question: 'Sure.',
+    shouting: 'Whoa, chill out!',
+    silence:  'Fine. Be that way!',
+    default:  'Whatever.'
+  }
+
   def self.hey(remark)
-    if remark.upcase == remark && remark.scan(/[a-zA-Z]/).empty? == false
-      'Whoa, chill out!'
-    elsif remark.rstrip.chars.last == '?'
-      'Sure.'
-    elsif remark.strip.empty?
-      'Fine. Be that way!'
+    RESPONSE[Remark.new(remark).type]
+  end
+end
+
+class Remark
+  def initialize(remark)
+    @remark = remark.strip
+  end
+
+  def type
+    if @remark.upcase == @remark && @remark.scan(/[a-zA-Z]/).empty? == false
+      :shouting
+    elsif @remark.chars.last == '?'
+      :question
+    elsif @remark.empty?
+      :silence
     else
-      'Whatever.'
+      :default
     end
   end
 end
