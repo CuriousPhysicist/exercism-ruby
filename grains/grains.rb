4c0d1685edec4a95d30934a8ef3227305d9c139d
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-26 16:12:19 +0100

    Grains challenge completed, Bob downloaded.

--- grains/grains.rb
+++ grains/grains.rb
@@ -1,21 +1,39 @@
+# below is an implememtation which fails the tests
+# but is a cleaner implementation of the Module
+
 # A module to calculate number of grains
 module Grains
-  BOARD = (1..64)
-  def self.square(position)
-    unless BOARD.member?(position)
+  def square(position)
+    unless positions.include?(position)
       raise ArgumentError,
-            "#{position} is not a position given on the chess board."
+            "\n#{position} is not a position given on the chess board.\n" +
+            "Choose between numbers #{positions}."
     end
     2**(position - 1)
   end
 
-  def self.total
-    BOARD.map do |position|
+  def total
+    positions.map do |position|
       square(position)
     end.reduce(:+)
+  end
+end
+
+# The Board Object
+class Board
+  include Grains
+  attr_reader :positions
+  def initialize(*dimensions)
+    @dimensions = dimensions
+    @size = dimensions.reduce(:*)
+    @positions = (1..@size)
   end
 end
 
 module BookKeeping
   VERSION = 1
 end
+
+p Board.new(8, 8).square(5)
+p Board.new(8, 8).total
+p Board.new(8, 8).square(65)
