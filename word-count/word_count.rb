39e789bbbdfe0e09499b712ca522c71faffa06cf
CuriousPhysicist <andrewhampson6@gmail.com>
2017-10-26 14:33:03 +0100

    Word Count challenge: Using Hash methods and procs to improve readability.

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -3,27 +3,11 @@ class Phrase
   attr_reader :phrase, :words
   def initialize(phrase)
     @phrase = phrase
-    find_words
-  end
-
-  def word_count
-    count_words.to_h
-  end
-
-  private
-
-  def find_words
     @words = phrase.downcase.scan(/\b[\w']+\b/)
   end
 
-  def count_words
-    words.map do |word|
-      [word, number_of(word)]
-    end
-  end
-
-  def number_of(word)
-    words.count { |wrd| wrd == word }
+  def word_count
+    words.group_by { |word| word }.transform_values(&:count)
   end
 end
 
